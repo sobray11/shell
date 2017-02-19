3ed63d3e2a349fded65bd9507bd73b2fa7480f1e
Scott Obray <sobray11@gmail.com>
2017-02-18 17:38:18 -0700

    Added history command

--- main.cpp
+++ main.cpp
@@ -16,22 +16,92 @@
 
 std::chrono::duration<double> timeSpent;
 double totalTime = 0.0;
-std::vector<std::string> history;
+std::vector<std::string> commandHistory;
+bool running = true;
 
 void ptime()
 {
     std::cout << "Time spent executing child processes: "
-        << std::chrono::duration_cast<std::chrono::seconds>(timeSpent).count()
-        << " seconds, " << std::chrono::duration_cast<std::chrono::milliseconds>(timeSpent).count()
-        << " miliseconds, and " << std::chrono::duration_cast<std::chrono::microseconds>(timeSpent).count()
+    << std::chrono::duration_cast<std::chrono::seconds>(timeSpent).count()
+    << " seconds, " << std::chrono::duration_cast<std::chrono::milliseconds>(timeSpent).count()
+    << " miliseconds, and " << std::chrono::duration_cast<std::chrono::microseconds>(timeSpent).count()
     << " microscends." << std::endl;
+}
+
+void history(std::string temp)
+{
+    if (!temp.compare("history"))
+    {
+        for (int i = 0; i < (int)commandHistory.size(); i++)
+        {
+            std::cout << commandHistory[i] << std::endl;
+        }
+    }
+    else
+    {
+        std::string historyNumber;
+        std::vector<std::string> words;
+        std::vector<char*> args;
+        for (int i = 2; i < (int)temp.size(); i++)
+        {
+            historyNumber.push_back(temp[i]);
+        }
+        int value = atoi(historyNumber.c_str());
+        if (value > (int)commandHistory.size())
+        {
+            std::cout << "Not enough entries in history list.\n";
+        }
+        else
+        {
+            auto cur = commandHistory[value].begin();
+            while (cur!= commandHistory[value].end())
+            {
+                auto next = std::find(cur, commandHistory[value].end(), ' ');
+                words.emplace_back(cur, next);
+                cur = std::find(cur, commandHistory[value].end(), ' ');
+                if (cur == commandHistory[value].end())
+                {
+                    break;
+                }
+                ++cur;
+            }
+            
+            
+            for (int i = 0; i < (int) words.size(); i++)
+            {
+                args.push_back((char*)words[i].c_str());
+            }
+            
+            auto start = std::chrono::steady_clock::now();
+            auto pid = fork();
+            if (pid < 0)
+            {
+                perror("error");
+                exit(EXIT_FAILURE);
+            }
+            if (pid == 0)
+            {
+                execvp(words[0].c_str(), args.data());
+            }
+            else
+            {
+                int status;
+                waitpid(pid, &status, 0);
+                auto end = std::chrono::steady_clock::now();
+                timeSpent = end - start;
+                totalTime += timeSpent.count();
+            }
+        }
+    }
+    
+    
 }
 
 int main()
 {
     
     
-    while (true)
+    while (running)
     {
         std::string line;
         std::vector<std::string> words;
@@ -39,9 +109,11 @@ int main()
         
         std::cout << "[cmd]: ";
         std::getline(std::cin, line);
+        commandHistory.push_back(line);
         
         if (!line.compare("exit"))
         {
+            running = false;
             std::cout << "Exiting Program\n";
             exit(EXIT_FAILURE);
             break;
@@ -49,6 +121,10 @@ int main()
         else if (!line.compare("ptime"))
         {
             ptime();
+        }
+        else if (!line.compare("history") || line[0] == '^')
+        {
+            history(line);
         }
         else
         {
@@ -68,7 +144,6 @@ int main()
             
             for (int i = 0; i < (int) words.size(); i++)
             {
-                //args.push_back(words[i].c_str());
                 args.push_back((char*)words[i].c_str());
             }
             
@@ -94,4 +169,3 @@ int main()
         }
     }
 }
-
